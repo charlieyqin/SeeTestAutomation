198a499a7f4bacfa927787e33c171ca461b24e72
kiran-anantapalli <kiran.anantapalli@pearson.com>
2016-05-23 16:16:07 -0400

    Login Object testcase added

--- SeeTest.Automation.MyApp/Tests/LoginTests.cs
+++ SeeTest.Automation.MyApp/Tests/LoginTests.cs
@@ -12,14 +12,40 @@ namespace SeeTest.Automation.EriBankTests
         [TestMethod]
         [TestCategory("Login")]
         [Priority(1)]
+        [WorkItem(1)]
         [Owner("Kiran Kumar Anantapalli(kiran.anantapalli)")]
         public void VerifySuccessfulLogin()
         {
-            using (loginAutomationAgent = new AutomationAgent("Verifying successful Login"))
+            using (loginAutomationAgent = new AutomationAgent("ID:1 - Verifying successful Login with strings"))
             {
                 try
                 {
                     LoginActions.Login(loginAutomationAgent, "company", "company");
+                    DashboardActions.ClickLogout(loginAutomationAgent);
+                }
+                catch (Exception ex)
+                {
+                    loginAutomationAgent.Sleep(2000);
+                    loginAutomationAgent.AddSteptoSeeTestReport(ex.Message, false);
+                    loginAutomationAgent.ApplicationClose();
+                    loginAutomationAgent.GenerateReportAndReleaseClient();
+                    throw;
+                }
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Login")]
+        [Priority(1)]
+        [WorkItem(2)]
+        [Owner("Kiran Kumar Anantapalli(kiran.anantapalli)")]
+        public void VerifyLoginObject()
+        {
+            using (loginAutomationAgent = new AutomationAgent("ID:2 - Verifying successful Login with Login Object"))
+            {
+                try
+                {
+                    LoginActions.Login(loginAutomationAgent, Login.GetLogin("DefaultUser"));
                     DashboardActions.ClickLogout(loginAutomationAgent);
                 }
                 catch (Exception ex)
